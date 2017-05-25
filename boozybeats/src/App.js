91a07452604215ff350863a09f3b3040dbb70164
Michael Crawford <mdcrawford96@gmail.com>
2017-05-25 14:42:48 -0400

    Working on app

--- boozybeats/src/App.js
+++ boozybeats/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import logo from './beerbeats.png';
+
 // let Sidebar = require('./Sidebar.js');
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>
-          Hello World!
-        </h1>
         <Sidebar />
       </div>
     );
@@ -19,7 +18,18 @@ class Sidebar extends Component {
   render() {
     return (
       // TODO
-      <p> Hello </p>
+      <div className="Sidebar">
+        <img className="Logo" src={logo} width="200" height="200" />
+        <p className="SiteName"> BOOZY BEATS </p>
+        <hr />
+        <table className="Links" width="100">
+          <tr> <th> HOME </th> </tr>
+          <tr> <th> ABOUT US </th> </tr>
+          <tr> <th> SOURCES </th> </tr>
+        </table>
+        <svg height="100%">
+        </svg>
+      </div>
     );
   }
 }
