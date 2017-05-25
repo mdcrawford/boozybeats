f6e29433e0b93bccb0436d1e0985d348da27b52f
Michael Crawford <mdcrawford96@gmail.com>
2017-05-25 13:08:13 -0400

    Started the design

--- boozybeats/src/App.js
+++ boozybeats/src/App.js
@@ -1,19 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// let Sidebar = require('./Sidebar.js');
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Hello World!
+        </h1>
+        <Sidebar />
       </div>
+    );
+  }
+}
+
+class Sidebar extends Component {
+  render() {
+    return (
+      // TODO
+      <p> Hello </p>
+    );
+  }
+}
+
+class SearchField extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // TODO
+    }
+  }
+
+  render() {
+    return (
+      // TODO
+      <div className="SearchField">
+      </div>
+    );
+  }
+
+}
+
+class Title extends Component {
+  render() {
+    return (
+      // TODO
+      <p> Hello </p>
+    );
+  }
+}
+
+class Legalese extends Component {
+  render() {
+    return (
+      // TODO
+      <p> Hello </p>
     );
   }
 }
